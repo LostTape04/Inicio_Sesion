2a76713215008d68290c00d56ed32746b9aa967d
LostTape04 <84998188+LostTape04@users.noreply.github.com>
2022-10-03 20:36:46 -0300

    Update inicio de sesion.py

--- inicio de sesion.py
+++ inicio de sesion.py
@@ -1,57 +1,60 @@
-from cProfile import label;
-import tkinter ; #LIBRERIA PARA LA INTERFAZ GRAFICA
-from tkinter import*;
-from tkinter import messagebox;
-from tkinter import font;
-import pymysql; #LIBRERIA PARA MANEJAR BASE DE DATOS
-#DEFINIMOS LA PANTALLA PRINCIPAL DEL PROGRAMA
+from tkinter import *
+from tkinter import messagebox
+import pymysql;
+
 def menu_pantalla():
-    global pantalla;
-    pantalla=Tk();
-    pantalla.geometry("400x480");
-    pantalla.title("Inicio");
-    pantalla.iconbitmap("UNDAV.ico");
-    image=PhotoImage(file="UNDAV.gif");
-    image=image.subsample(1,1);
-    label=Label(image=image);
-    label.pack();
-    Label(text="Acceso al sistema", bg="navy", fg="white", width="300", height="3", font=("calibrí", 15)).pack();
-    Label(text="").pack();
-    Button(text="Inicio de Sesion", height="3", width="30", command=inicio_sesion).pack();
-    Label(text="").pack();
-    Button(text="Registrarse", height="3", width="30", command=registrar).pack();
-    pantalla.mainloop();
+    global pantalla
+    pantalla=Tk()
+    pantalla.geometry("400x480")
+    pantalla.title("bienvenidos")
+    pantalla.iconbitmap("UNDAV.ico")
 
-#DEFINIMOS LA PANTALLA DE INICIO
-def inicio_sesion():
-    global pantalla1;
-    pantalla1 = Toplevel(pantalla);
-    pantalla1.geometry("400x480");
-    pantalla1.title("Iniciar Sesión");
-    pantalla1.iconbitmap("UNDAV.ico");
-    Label(pantalla1, text="Ingrese su Usuario y Contraseña a continuación", bg="navy", fg="white", width="150", height="2", font=("calibrí", 10)).pack();
-    Label(pantalla1, text=" ").pack;
-    global nombre_usuario_verify;
-    global contrasenausuario_verify;
+    image = PhotoImage(file="UNDAV.gif")
+    image = image.subsample(1, 1)
+    label = Label(image=image)
+    label.pack()
 
-    nombre_usuario_verify = StringVar();
-    contrasenausuario_verify = StringVar();
+    Label(text="Acceso al sistema",bg="navy", fg="white", width="300", height="3", font=("Calibri", 15)).pack()
+    Label(text="").pack()
 
-    global nombre_usuario_entry;
-    global contrasena_usuario_entry;
+    Button(text="Iniciar Sesión", command=incio_sesion, height="3", width="30").pack()
+    Label(text="").pack()
 
-    Label(pantalla1, text="Usuario").pack();
-    nombre_usuario_entry = Entry(pantalla1, textvariable=nombre_usuario_verify);
-    nombre_usuario_entry.pack();
-    Label(pantalla1).pack();
+    Button(text="registrar", command=registrar, height="3", width="30").pack()
 
-    Label(pantalla1, text="Contraseña").pack();
-    contrasena_usuario_entry = Entry(pantalla1, textvariable=contrasenausuario_verify);
-    contrasena_usuario_entry.pack();
-    Label(pantalla1).pack();
+    pantalla.mainloop()
 
-    Button(pantalla1, text="Iniciar sesion").pack();
-#DEFINIMOS LA PANTALLA DE REGISTRO
+def incio_sesion():
+    global pantalla1
+    pantalla1 = Toplevel(pantalla)
+    pantalla1.geometry("400x480")
+    pantalla1.title("Inicio de sesion")
+    pantalla1.iconbitmap("UNDAV.ico")
+
+    Label(pantalla1, text="Ingrese Usuario y Contraseña", bg="navy", fg="white", width="300", height="3", font=("Calibri", 10)).pack()
+    Label(pantalla1, text="").pack()
+
+    global nombreusuario_verify
+    global contrasenausuario_verify
+
+    nombreusuario_verify = StringVar()
+    contrasenausuario_verify = StringVar()
+
+    global nombre_usuario_entry
+    global contrasena_usuario_entry
+
+    Label(pantalla1, text="Usuario").pack()
+    nombre_usuario_entry = Entry(pantalla1, textvariable=nombreusuario_verify)
+    nombre_usuario_entry.pack()
+    Label(pantalla1).pack()
+
+    Label(pantalla1, text="Contraseña").pack()
+    contrasena_usuario_entry = Entry(pantalla1, textvariable=contrasenausuario_verify)
+    contrasena_usuario_entry.pack()
+    Label(pantalla1).pack()
+
+    Button(pantalla1, text="Iniciar sesion").pack()
+
 def registrar():
     global pantalla2;
     pantalla2 = Toplevel (pantalla);
@@ -60,43 +63,43 @@ def registrar():
     pantalla2.iconbitmap("UNDAV.ico");
     
     global nombreusuario_entry;
-    global contrasenausuario_entry;
+    global contrasena_entry;
 
-    nombreusuario_entry = StringVar;
-    contrasenausuario_entry = StringVar;
+    nombreusuario_entry=StringVar();
+    contrasena_entry=StringVar();
 
-    Label(pantalla2, text="Ingrese un Usuario y Contraseña", bg="navy", fg="white", width="150", height="2", font=("calibrí", 10)).pack();
-    Label(pantalla2, text="");
-    Label(pantalla2, text="Usuario").pack();
-    nombreusuario_entry = Entry(pantalla2).pack();
+    Label(pantalla2, text="Ingrese un Usuario y contraseña para el registro al sistema",bg="navy", fg="white", width="300", height="3", font=("Calibri", 10)).pack()
+    Label(pantalla2, text="").pack();
+
+    Label(pantalla2, text="Usuario").pack()
+    nombreusuario_entry = Entry(pantalla2)
+    nombreusuario_entry.pack()
+    Label(pantalla2).pack();
+
     Label(pantalla2, text="Contraseña").pack()
-    contrasenausuario_entry = Entry(pantalla2).pack();
-    Button(pantalla2, text="Registrarse", command=inserta_datos).pack();
-#DEFINIMOS LA FUNCION PARA QUE AL MOMENTO DE DAR CLICK EN EL BOTON DE REGISTRARSE SUBA LOS DATOS DEL USUARIO Y CONTRASEÑA
-def inserta_datos():
+    contrasena_entry = Entry(pantalla2)
+    contrasena_entry.pack()
+    Label(pantalla2).pack()
+    Button(pantalla2, text="Registrarse", command=insertar_datos).pack()
+    
+def insertar_datos():
+    bd=pymysql.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        db="bd2"
+    )
+    fcursor=bd.cursor();
+    sql="INSERT INTO login (usuario, contrasena) VALUES ('{0}','{1}')".format(nombreusuario_entry.get(), contrasena_entry.get());
+    
+    try:
+        fcursor.execute(sql);
+        bd.commit();
+        messagebox.showinfo(message="Registro exitoso", title="Aviso")
+    except:
+        bd.rollback();
+        messagebox.showinfo(message="NO Registrado", title="Aviso");
+        bd.close();
 
 
-		bd = pymysql.connect(
-			host ="localhost",
-			user ="root",
-			password ="",
-			db ="bd2")
-
-		fcursor = bd.cursor()
-		
-		sql="INSERT INTO login (usuario,  contrasena) VALUES ('{0}', '{1}')".format(nombreusuario_entry.get(), contrasenausuario_entry.get());
-
-		try:
-			fcursor.execute(sql)
-			bd.commit()
-			messagebox.showinfo(message="Registro Exitoso", title="Aviso")
-
-		except: 
-			bd.rollback()
-			messagebox.showinfo(message="No pudo registrase", title="Aviso")
-
-		bd.close()
-
-
-
-menu_pantalla();
+menu_pantalla()
